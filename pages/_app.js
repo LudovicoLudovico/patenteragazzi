5fed9965fbeed398e6e4934d616339acb692e672
ludovico <ludovico.furlanetto@gmail.com>
2020-12-22 12:28:51 +0100

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,21 @@ import QuestionsProvider from '../context/questionsContext';
 import AdminProvider from '../context/adminContext';
 import '../main.min.css';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <UserProvider>
       <QuestionsProvider>
