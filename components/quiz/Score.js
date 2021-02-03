dd9159476bf9f02e23e1f631174a4cfb71d42182
ludovico <ludovico.furlanetto@gmail.com>
2021-02-03 16:47:57 +0100

    mobile fix

--- components/quiz/Score.js
+++ components/quiz/Score.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import WrongAnswer from './WrongAnswer';
 import Link from 'next/link';
+import { Button } from '@material-ui/core';
 import '../../style/score.min.css';
 import dynamic from 'next/dynamic';
 const AdBanner = dynamic(() => import('../AdBanner'), {
@@ -20,103 +21,102 @@ const Score = ({
     <>
       {showScore && (
         <div className='score'>
-          <div className='score_top'>
-            <div>
-              <h1>
-                <p>Risultato</p> {score}/40
-                <p>- {score >= 36 ? 'Promosso' : 'Bocciato'}</p>
-              </h1>
+          <div className='score_content'>
+            <div className='score_top'>
+              <div className='score_top_left'>
+                <h1>
+                  <p>Risultato</p> {score}/40
+                  <p>- {score >= 36 ? 'Promosso' : 'Bocciato'}</p>
+                </h1>
+                <Link href='/'>
+                  <a style={{ textDecoration: 'none' }}>
+                    <Button className='close_quiz'>ESCI</Button>
+                  </a>
+                </Link>
+              </div>
+
+              <div className='score_nav'>
+                <div
+                  onClick={() => setIsActive(0)}
+                  className={`${isActive == 0 ? 'active' : ''}`}
+                >
+                  Sbagliate
+                </div>
+                <div
+                  onClick={() => setIsActive(1)}
+                  className={`${isActive == 1 ? 'active' : ''}`}
+                >
+                  Non date
+                </div>
+                <div
+                  onClick={() => setIsActive(2)}
+                  className={`${isActive == 2 ? 'active' : ''}`}
+                >
+                  Tutte
+                </div>
+              </div>
             </div>
 
-            <Link href='/'>
-              <a>
-                <button className='close_quiz'>x</button>
-              </a>
-            </Link>
-          </div>
-
-          <div className='separator'></div>
-          <div className='score_nav'>
-            <div
-              onClick={() => setIsActive(0)}
-              className={`${isActive == 0 ? 'active' : ''}`}
-            >
-              Sbagliate
-            </div>
-            <div
-              onClick={() => setIsActive(1)}
-              className={`${isActive == 1 ? 'active' : ''}`}
-            >
-              Non date
-            </div>
-            <div
-              onClick={() => setIsActive(2)}
-              className={`${isActive == 2 ? 'active' : ''}`}
-            >
-              Tutte le domande
-            </div>
-          </div>
-
-          <div className='wrong_answer_container'>
-            {isActive == 0 && (
-              <>
-                {wrongAnswers.map((wrong, index) => {
-                  return (
-                    <WrongAnswer
-                      key={index}
-                      wrong={wrong}
-                      index={index}
-                      isAllQuestions={false}
-                      answers={answers}
-                      theory={theory.filter((theory) => {
-                        return theory.id == wrong.answer;
-                      })}
-                    />
-                  );
-                })}
-              </>
-            )}
-            {isActive == 1 && (
-              <>
-                {wrongAnswers
-                  .filter((wrongItem) => wrongItem.userResponse == null)
-                  .map((wrong, index) => {
+            <div className='wrong_answer_container'>
+              {isActive == 0 && (
+                <>
+                  {wrongAnswers.map((wrong, index) => {
                     return (
                       <WrongAnswer
                         key={index}
                         wrong={wrong}
                         index={index}
-                        answers={answers}
                         isAllQuestions={false}
+                        answers={answers}
                         theory={theory.filter((theory) => {
                           return theory.id == wrong.answer;
                         })}
                       />
                     );
                   })}
-              </>
-            )}
+                </>
+              )}
+              {isActive == 1 && (
+                <>
+                  {wrongAnswers
+                    .filter((wrongItem) => wrongItem.userResponse == null)
+                    .map((wrong, index) => {
+                      return (
+                        <WrongAnswer
+                          key={index}
+                          wrong={wrong}
+                          index={index}
+                          answers={answers}
+                          isAllQuestions={false}
+                          theory={theory.filter((theory) => {
+                            return theory.id == wrong.answer;
+                          })}
+                        />
+                      );
+                    })}
+                </>
+              )}
 
-            {isActive == 2 && (
-              <>
-                {quizQuestions.map((wrong, index) => {
-                  return (
-                    <WrongAnswer
-                      key={index}
-                      index={index}
-                      wrong={wrong}
-                      isAllQuestions={true}
-                      answers={answers}
-                      theory={theory.filter((theory) => {
-                        return theory.id == wrong.answer;
-                      })}
-                    />
-                  );
-                })}
-              </>
-            )}
+              {isActive == 2 && (
+                <>
+                  {quizQuestions.map((wrong, index) => {
+                    return (
+                      <WrongAnswer
+                        key={index}
+                        index={index}
+                        wrong={wrong}
+                        isAllQuestions={true}
+                        answers={answers}
+                        theory={theory.filter((theory) => {
+                          return theory.id == wrong.answer;
+                        })}
+                      />
+                    );
+                  })}
+                </>
+              )}
+            </div>
           </div>
-
           <div className='score_banner'>
             <AdBanner />
           </div>
