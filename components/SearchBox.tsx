c16be8bf8149fb167f2846ffae0ca35ac4b49948
ludovico <ludovico.furlanetto@gmail.com>
2021-02-04 17:51:44 +0100

    fixed types 2

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 
 interface SearchBoxProps {
-  filters: string[];
+  filters?: string[] | string;
   setFilters: (string: string) => void;
 }
 
