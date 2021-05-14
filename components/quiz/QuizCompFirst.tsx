89f4f08f0ef1a5ce53bdbcb52cac0b72b35c089a
Ludovico Furlanetto <ludovico.furlanetto@gmail.com>
2021-05-14 16:46:25 +0200

    fixed twice sim

--- components/quiz/QuizCompFirst.tsx
+++ components/quiz/QuizCompFirst.tsx
@@ -16,76 +16,80 @@ const QuizComp = memo(({ question, getAnswer, index }: QuizCompProps) => {
 
   return (
     <>
-      <div className='quiz_image'>
-        {question.image ? (
-          <>
-            <Modal
-              open={open}
-              onClick={() => setOpen(false)}
-              aria-labelledby='simple-modal-title'
-              aria-describedby='simple-modal-description'
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                outline: 'none',
-              }}
+      {question && (
+        <>
+          <div className='quiz_image'>
+            {question.image ? (
+              <>
+                <Modal
+                  open={open}
+                  onClick={() => setOpen(false)}
+                  aria-labelledby='simple-modal-title'
+                  aria-describedby='simple-modal-description'
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    outline: 'none',
+                  }}
+                >
+                  <img
+                    src={question.image}
+                    alt=''
+                    className='modal_img'
+                    style={{
+                      boxSizing: 'border-box',
+                      height: 'auto',
+                      width: '100%',
+                      maxWidth: 700,
+                      padding: 20,
+                      margin: 10,
+                      border: '5px solid white',
+                      borderRadius: 20,
+                      background: '#00408b',
+                      outline: 'none',
+                    }}
+                  />
+                </Modal>
+
+                <img
+                  onClick={() => setOpen(true)}
+                  src={question.image}
+                  alt='Caricamento...'
+                />
+              </>
+            ) : (
+              <div className='modal_img'></div>
+            )}
+          </div>
+
+          {/* Question */}
+          <div className='quiz_question'>
+            <p>
+              {question.question.charAt(0).toUpperCase() +
+                question.question.slice(1)}
+            </p>
+          </div>
+
+          {/* Buttons for answering */}
+          <div className='quiz_answer'>
+            <Button
+              variant='contained'
+              className='quiz_answer_btn'
+              onClick={() => getAnswer(index, true, false)}
             >
-              <img
-                src={question.image}
-                alt=''
-                className='modal_img'
-                style={{
-                  boxSizing: 'border-box',
-                  height: 'auto',
-                  width: '100%',
-                  maxWidth: 700,
-                  padding: 20,
-                  margin: 10,
-                  border: '5px solid white',
-                  borderRadius: 20,
-                  background: '#00408b',
-                  outline: 'none',
-                }}
-              />
-            </Modal>
-
-            <img
-              onClick={() => setOpen(true)}
-              src={question.image}
-              alt='Caricamento...'
-            />
-          </>
-        ) : (
-          <div className='modal_img'></div>
-        )}
-      </div>
-
-      {/* Question */}
-      <div className='quiz_question'>
-        <p>
-          {question.question.charAt(0).toUpperCase() +
-            question.question.slice(1)}
-        </p>
-      </div>
-
-      {/* Buttons for answering */}
-      <div className='quiz_answer'>
-        <Button
-          variant='contained'
-          className='quiz_answer_btn'
-          onClick={() => getAnswer(index, true, false)}
-        >
-          Vero
-        </Button>
-        <Button
-          variant='contained'
-          className='quiz_answer_btn'
-          onClick={() => getAnswer(index, false, false)}
-        >
-          Falso
-        </Button>
-      </div>
+              Vero
+            </Button>
+            <Button
+              variant='contained'
+              className='quiz_answer_btn'
+              onClick={() => getAnswer(index, false, false)}
+            >
+              Falso
+            </Button>
+          </div>
+        </>
+      )}
     </>
   );
 });
--- components/quizSim/QuizSimComp.tsx
+++ components/quizSim/QuizSimComp.tsx
@@ -13,44 +13,50 @@ const QuizSimComp = ({ index, question, getAnswer }: QuizSimCompProps) => {
   const [answer, setAnswer] = useState<boolean>();
   return (
     <>
-      <div className='quiz-sim_box_image'>
-        {question.image && <img src={question.image} alt='Caricamento...' />}
-      </div>
-      <div className='quiz-sim_box_question'>
-        <p className='quiz-sim_box_question_num'>{index + 1}</p>
-        <p className='quiz-sim_box_question_question'>
-          {question.question.charAt(0).toUpperCase() +
-            question.question.slice(1)}
-        </p>
-      </div>
-      <div className='quiz-sim_box_answers'>
+      {question && (
         <>
-          <img
-            src={` ${
-              answer == null || answer == false
-                ? '/true.svg'
-                : '/true-crossed.svg'
-            }`}
-            alt=''
-            onClick={() => {
-              getAnswer(index, true, true);
-              setAnswer(true);
-            }}
-          />
-          <img
-            src={` ${
-              answer == null || answer == true
-                ? '/false.svg'
-                : '/false-crossed.svg'
-            }`}
-            alt=''
-            onClick={() => {
-              getAnswer(index, false, true);
-              setAnswer(false);
-            }}
-          />
+          <div className='quiz-sim_box_image'>
+            {question.image && (
+              <img src={question.image} alt='Caricamento...' />
+            )}
+          </div>
+          <div className='quiz-sim_box_question'>
+            <p className='quiz-sim_box_question_num'>{index + 1}</p>
+            <p className='quiz-sim_box_question_question'>
+              {question.question.charAt(0).toUpperCase() +
+                question.question.slice(1)}
+            </p>
+          </div>
+          <div className='quiz-sim_box_answers'>
+            <>
+              <img
+                src={` ${
+                  answer == null || answer == false
+                    ? '/true.svg'
+                    : '/true-crossed.svg'
+                }`}
+                alt=''
+                onClick={() => {
+                  getAnswer(index, true, true);
+                  setAnswer(true);
+                }}
+              />
+              <img
+                src={` ${
+                  answer == null || answer == true
+                    ? '/false.svg'
+                    : '/false-crossed.svg'
+                }`}
+                alt=''
+                onClick={() => {
+                  getAnswer(index, false, true);
+                  setAnswer(false);
+                }}
+              />
+            </>
+          </div>
         </>
-      </div>
+      )}
     </>
   );
 };
